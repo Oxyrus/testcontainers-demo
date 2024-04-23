ff508060991d6593ccf246e632e7d27f0089d2ce
Oxyrus <im.oxyrus@gmail.com>
2024-04-23 11:16:11 -0500

    Initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# Testcontainers Demo
+
+Repository contains two use cases, container for multiple tests in the same file (`repo_test.go`) and multiple tests in a single file where we want to reuse the same container (`repo_suite_test.go`).
+
+## Execution
+
+Run tests with `$ go test ./... -v`
---
+++ internal/customer/repo.go
@@ -0,0 +1,42 @@
+package customer
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+)
+
+type CustomerRepository struct {
+	conn *pgx.Conn
+}
+
+func NewRepository(ctx context.Context, connStr string) (*CustomerRepository, error) {
+	conn, err := pgx.Connect(ctx, connStr)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		return nil, err
+	}
+
+	return &CustomerRepository{conn: conn}, nil
+}
+
+func (r CustomerRepository) CreateCustomer(ctx context.Context, customer Customer) (Customer, error) {
+	err := r.conn.QueryRow(ctx,
+		"INSERT INTO customers (name, email) VALUES ($1, $2) RETURNING id",
+		customer.Name, customer.Email).Scan(&customer.Id)
+	return customer, err
+}
+
+func (r CustomerRepository) GetCustomerByEmail(ctx context.Context, email string) (Customer, error) {
+	var customer Customer
+
+	query := "SELECT id, name, email FROM customers WHERE email = $1"
+	err := r.conn.QueryRow(ctx, query, email).Scan(&customer.Id, &customer.Name, &customer.Email)
+	if err != nil {
+		return Customer{}, err
+	}
+
+	return customer, nil
+}
